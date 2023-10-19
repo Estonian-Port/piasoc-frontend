00c52d8ed16cf1255a3aa726faf046a11570286f
RdzSebastian <rdzsebastian@gmail.com>
2023-10-19 12:38:14 -0300

    Agrega bootstrap pagina cotizacion y seguros, ajusta navbar y style.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrincipalComponent } from './layout/principal/principal.component'
+import { PrincipalComponent } from './layout/principal/principal.component';
+import { CotizacionComponent } from './layout/cotizacion/cotizacion.component';
+import { SegurosComponent } from './layout/seguros/seguros.component';
+import { NavbarComponent } from './components/navbar/navbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    CotizacionComponent,
+    SegurosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
